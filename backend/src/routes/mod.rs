b92290124f9b111250d94cb87a1dcafe8b86dc12
Your Name <you@example.com>
2026-04-05 19:19:58 -0400

    fix gitignore

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -1,2 +1,17 @@
+use axum::response::IntoResponse;
+use axum::http::StatusCode;
+
+use crate::ops::OpError;
+
 pub mod auth;
 pub mod filesystem;
+
+impl IntoResponse for OpError {
+    fn into_response(self) -> axum::response::Response {
+        match self {
+            Self::Generic(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()).into_response(),
+            Self::EntityConflict { reason } => (StatusCode::CONFLICT, reason).into_response(),
+            Self::UserNotLoggedIn => (StatusCode::UNAUTHORIZED, "You must be logged in to perform this operation").into_response()
+        }
+    }
+}
